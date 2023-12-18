98d2f6cdc9f13a796b1f6ba2b9ec6e6f2d4b7458
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-12-18 15:11:49 +0200

    Create Contacts.styled.jsx

---
+++ src/components/Contacts/Contacts.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ListWrapper = styled.ul`
+    background-color: rosybrown;
+`;
+
+export const ListItem = styled.li`
+    line-height: 0.8;
+`;
